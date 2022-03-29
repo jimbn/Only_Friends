9b172db89dc6960527ae185c4299af2c22a64bac
SLEEPlESS <rosas_u@ymail.com>
2022-03-29 16:46:34 -0700

    added category and channel model files

--- models/Post.js
+++ models/Post.js
@@ -7,7 +7,22 @@ class Post extends Model{}
 
 // Initiates models and holds properties
 Post.init({
-
+    id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    // address string length in the future for title
+    title:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    category:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        
+    }
 });
 
 module.exports = Post;
