56a45963fee5dcc8b0a8c1150e892c0d85a0c3bb
SLEEPlESS <rosas_u@ymail.com>
2022-03-29 20:22:34 -0700

    finished the post model

--- models/Post.js
+++ models/Post.js
@@ -6,6 +6,7 @@ const sequlize = require('../config/connection');
 class Post extends Model{}
 
 // Initiates models and holds properties
+// figure out how to male the model have media in the prop
 Post.init({
     id:{
         type: DataTypes.INTEGER,
@@ -18,11 +19,41 @@ Post.init({
         type: DataTypes.STRING,
         allowNull: false
     },
-    category:{
+    preview:{
         type: DataTypes.STRING,
-        allowNull: false,
+        // make it show the first few parts from the post
+        allowNull: false
+    },
+    Post_body: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    // this is our foreign key for our reference 
+    category_id:{
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'ategory',
+            key: 'id'
+        }
         
+    },
+    // this is our foreign key for our reference 
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    },
+    // this is our foreign key
+    channel_id:{
+        type: DataTypes.INTEGER,  
+        references: {
+            model:'channel',
+            key: 'id'
+        }      
     }
+
 });
 
 module.exports = Post;
