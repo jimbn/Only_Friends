0d5eabb3ae8bda267890e9baef18a32abf8db615
SLEEPlESS <rosas_u@ymail.com>
2022-03-29 20:47:22 -0700

    finished post associations and added category and channel route files in the controller/api folder

--- models/index.js
+++ models/index.js
@@ -1,24 +1,43 @@
+// required models for associations
 const User = require('./User');
-const Post = require('./Post')
+const Post = require('./Post');
 const Comment = require('./Comment');
+const Channel = require('./Channel');
+const Category =require('./Category');
 
+// associatyions for user model
 Comment.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
+// associatyions for user model
 Comment.belongsTo(Post, {
     foreignKey: 'post_id'
 });
 
+// associatyions for user model
 User.hasMany(Comment, {
     foreignKey: 'user_id'
 });
 
+// associatyions for user model
 Post.hasMany(Comment, {
     foreignKey: 'post_id'
 });
+Post.belongsTo(Category, {
+    foreignKey: 'category_id'
+});
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+Post.belongsTo(Channel, {
+    foreignKey: 'channel_id'
+});
+
+// TO DO: associatyions for category model
 
 
 
 
-module.exports = User, Post, Comment; 
+
+module.exports = {User, Post, Comment, Channel, Category}; 
