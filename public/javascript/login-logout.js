0639217de1c8ba118bc1a393e0aee700761e32bb
SLEEPlESS <rosas_u@ymail.com>
2022-04-01 20:05:30 -0700

    made correction to endpoint and added comments

--- public/javascript/login-logout.js
+++ public/javascript/login-logout.js
@@ -7,16 +7,18 @@
 async function logout(){
     // this is to test to see if the button triggers the function
     console.log('You clicked the logout button')
-   
+//    makes request to logout route in api user-routes
     const logoutResponse = await fetch('/api/user/logout', {
         method: 'post',
         headers: { 'Content-Type': 'application/json' }
     });
-
+// if the reesponse is good then redirects user to home route  '/'
     if(logoutResponse.ok){
         document.location.replace('/');
     }else {
+        // responds with error code
         alert(response.statusText);
     }
 }
+// listening for log out button click
 document.querySelector('#logout').addEventListener('click', logout);
