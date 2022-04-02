bf5c524989e7cb25d8ad21fbe8c019265e3e9105
Michael Cortez-Mejia <you@example.com>
2022-04-01 20:07:15 -0700

    API post routes, GET added

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,10 +1,39 @@
 const router = require("express").Router();
-const { Post } = require('../../models');
+const { User, Post, Comment } = require('../../models');
 
-// create Post
+// get all posts
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'post_body',
+            'category_name',
+            'created_at'
+        ],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: Comment, 
+                attributes: ['id', 'comment_text', 'user_id', 'post_id', 'created_at'],
+                include: {
+                    // username of commenter
+                    model: User, 
+                    attributes: ['username']
+                }
+            },
+            {
+                // owner of post
+                model: User, 
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(postData => res.json(postData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
 
-// read post 
-
-// update post 
-
-// delete post 
+module.exports = router; 
--- models/Post.js
+++ models/Post.js
@@ -7,52 +7,45 @@ class Post extends Model { }
 
 // Initiates models and holds properties
 // figure out how to make the model have media in the prop. consider using urls. Save the urls as strings
-Post.init({
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    // address string length in the future for title
-    title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-         //   len is for the length of characters min 2 and a max of 50
-         len: [2,50]
-    },
-    post_body: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    },
-    category_name:{
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    // this is our foreign key for our reference 
-    user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: 'user',
-            key: 'id'
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // address string length in the future for title
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            //   len is for the length of characters min 2 and a max of 50
+            len: [2,50]
+        },
+        post_body: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        category_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // this is our foreign key for our reference 
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
         }
     },
-    // this is our foreign key
-    // channel_id: {
-    //     type: DataTypes.INTEGER,
-    //     references: {
-    //         model: 'channel',
-    //         key: 'id'
-    //     }
-    // }
-},
-    {
-        sequelize,
-        timeStamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'post'
-    }
+        {
+            sequelize,
+            timeStamps: false,
+            freezeTableName: true,
+            underscored: true,
+            modelName: 'post'
+        }
 );
 
 module.exports = Post;
