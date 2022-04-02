f8e523889f95b3e54f0da1c76a0fe7a040624fbc
SLEEPlESS <rosas_u@ymail.com>
2022-04-01 19:59:17 -0700

    made log out fetch request" /

--- public/javascript/login-logout.js
+++ public/javascript/login-logout.js
@@ -1,3 +1,22 @@
 // this gets connected to the handelbars file with a script tag
 
 // login handler function
+
+
+// function to handle logout functionality
+async function logout(){
+    // this is to test to see if the button triggers the function
+    console.log('You clicked the logout button')
+   
+    const logoutResponse = await fetch('/api/user/logout', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if(logoutResponse.ok){
+        document.location.replace('/');
+    }else {
+        alert(response.statusText);
+    }
+}
+document.querySelector('#logout').addEventListener('click', logout);
