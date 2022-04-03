4277f10e69561d962f23f85c0fa02999000ffdff
Jim Nguyen <95522881+jimbn@users.noreply.github.com>
2022-04-03 01:36:08 -0700

    set up signup modal

--- public/javascript/util/modal.js
+++ public/javascript/util/modal.js
@@ -1,7 +1,14 @@
+// const for login modal
 const loginBtn = document.querySelector("#open-login-modal");
 const loginModal = document.querySelector(".loginModal");
-const closeLoginModal = document.querySelector("#login-btn")
+const closeLoginModal = document.querySelector("#login-btn");
 
+//const for signup modal
+const signupBtn = document.querySelector("#open-signup-modal");
+const signupModal = document.querySelector(".signupModal");
+const closeSignupModal = document.querySelector("#signup-btn");
+
+// function to display and hide login modal
 loginBtn.addEventListener("click", function() {
     loginModal.classList.add("modal");
 });
@@ -9,7 +16,10 @@ closeLoginModal.addEventListener("click", function() {
     loginModal.classList.remove("modal");
 });
 
-// closeModalBtn.addEventListener(`click`,function() {
-//     locError.classList.remove(`modal`);
-// });
-
+// function to display and hide signup modal
+signupBtn.addEventListener("click", function() {
+    signupModal.classList.add("modal");
+});
+closeSignupModal.addEventListener("click", function() {
+    signupModal.classList.remove("modal");
+});
