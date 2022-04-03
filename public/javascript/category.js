533ea154bc8e5ec3d5a9798472b036df76c291c3
SLEEPlESS <rosas_u@ymail.com>
2022-04-03 00:04:34 -0700

    made category fetch request to search post by category

---
+++ public/javascript/category.js
@@ -0,0 +1,28 @@
+// this function will handle redirecting the user to a post based on the category clicked 
+async function categorySearch(event) {
+    // Extract the category name from the element that was clicked
+    const categoryName = this.value;
+    console.log(categoryName);
+    if (categoryName) {
+        // gets data for category
+        // NOTE this route may need /api to be changed
+        const response = await fetch(`api/post/${categoryName}`, {
+            method: 'POST',
+            body: JSON.stringify({
+                title,
+                post_body
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if(response.ok) {
+            // NOTE this may need a different route
+            document.location.replace('/posts-by-category')
+        }
+    }
+}
+
+
+
+document.queryCommandIndeterm('.search-by-category').on('click', categorySearch);
