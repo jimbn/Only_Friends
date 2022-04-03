f754960b63e21b4799661110bf681723d5b46fa7
Jim Nguyen <95522881+jimbn@users.noreply.github.com>
2022-04-03 01:02:08 -0700

    add modal css and partial for login and signup

---
+++ public/javascript/util/modal.js
@@ -0,0 +1,6 @@
+$(".login-btn").click(function() {$(".loginModal").removeClass("modalHide").addClass("modal")})
+
+closeModalBtn.addEventListener(`click`,function() {
+    locError.classList.remove(`modal`);
+});
+
