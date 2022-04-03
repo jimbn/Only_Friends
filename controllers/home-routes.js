923406cc5c17ef9c66f98712cc91089e3f51bd39
HBK <sammcowen@gmail.com>
2022-04-02 23:05:17 -0700

    creating post-bycategory homeroute and view . route is not grabbing the data

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -66,6 +66,67 @@ router.get('/post/:id', (req,res) => {
     });
 });
 
+// route for when user clicks on a category name from hompeage aka landing page 
+router.get('/post/category/:category_name', (req,res) => {
+   
+    Post.findAll({
+        
+        
+        where: {
+            category_name: req.params.category_name
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_body',
+            'category_name',
+            'created_at'
+        ],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: Comment, 
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'user_id',
+                    'post_id',
+                    'created_at'
+                ],
+                include: {
+                    // username of commenter
+                    model: User, 
+                    attributes: ['username']
+                }
+            },
+            {
+                // owner of post
+                model: User, 
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(postData =>{
+        if(!postData) {
+            res.status(404).json({message:"no post found with this id"});
+            return;
+        }
+        // serialzie the post data 
+        const posts = postData.get({plain:true});
+
+        // pass data to template 
+        res.render('posts-by-category',{
+            posts
+        // loggedIn:req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
 // login route to renderlogin.handlebars
 router.get('/login', (req,res) => {
     // check for login and return to homepage if logged in
