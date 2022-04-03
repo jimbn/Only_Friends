212b88c95587f7a4b13ca77b1a30fb8734333a39
Michael Cortez-Mejia <you@example.com>
2022-04-02 18:03:10 -0700

    comment routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,80 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require('../../models');
+
+
+// GET all comments 
+router.get('/', (req, res) => {
+    Comment.findAll({
+        attributes: [
+            'id',
+            'comment_text',
+            'user_id',
+            'post_id'
+        ],
+        include: {
+            model: User, 
+            attributes: ['username']
+        }
+    })
+    .then(commentData => res.json(commentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+// GET 1 post by id 
+router.get('/:id', (req, res) => {
+    Comment.findAll({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'comment_text',
+            'user_id',
+            'post_id'
+        ],
+        include:[ 
+            {
+                model: User, 
+                attributes: ['username']
+            },
+            {
+                model: Post,
+                attributes: ['post_body'],
+                include: {
+                    model: User,
+                    attributes: [['username', 'post_owner']]
+                }
+            }
+        ]
+    })
+    .then(commentData => res.json(commentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+
+// POST create a Comment
+router.post('/', (req, res) => {
+    Comment.create({
+    // uncomment if statment and req.session.user_id once we are working it into the front end 
+    // if(req.session) {
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id /* req.session.user_id*/,
+        post_id: req.body.post_id
+    //}
+    })
+    .then(commentData => res.json(commentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+
+
+module.exports = router;
