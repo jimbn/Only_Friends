2e3b575949f29967055e52bf8f7facb19e7af044
Jim Nguyen <95522881+jimbn@users.noreply.github.com>
2022-04-04 03:26:30 -0700

    add js to hide overflow when modal is open

--- public/javascript/util/modal.js
+++ public/javascript/util/modal.js
@@ -6,20 +6,26 @@ const signupBtn = document.querySelector("#open-signup-modal");
 const signupModal = document.querySelector("#signupModal");
 const closeSignupModal = document.querySelector("#signup-btn");
 
+const body = document.querySelector("body");
+
 // function to display and hide login modal
 loginBtn.addEventListener("click", function() {
     loginModal.classList.add("modal");
+    body.style.overflow="hidden";
 });
 
 closeLoginModal.addEventListener("click", function() {
     loginModal.classList.remove("modal");
+    body.style.overflow="visible";
 });
 
 // function to display and hide signup modal
 signupBtn.addEventListener("click", function() {
     signupModal.classList.add("modal");
+    body.style.overflow="hidden";
 });
 
 closeSignupModal.addEventListener("click", function() {
     signupModal.classList.remove("modal");
+    body.style.overflow="visible";
 });
