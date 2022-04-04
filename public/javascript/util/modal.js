8b7dc4c2d247f0d14a28bc6ec7c5dee88612af58
Jim Nguyen <95522881+jimbn@users.noreply.github.com>
2022-04-03 18:10:26 -0700

    fixed signup modal. modal open and close correctly

--- public/javascript/util/modal.js
+++ public/javascript/util/modal.js
@@ -1,17 +1,16 @@
-// const for login modal
 const loginBtn = document.querySelector("#open-login-modal");
-const loginModal = document.querySelector(".loginModal");
+const loginModal = document.querySelector("#loginModal");
 const closeLoginModal = document.querySelector("#login-btn");
 
-//const for signup modal
 const signupBtn = document.querySelector("#open-signup-modal");
-const signupModal = document.querySelector(".signupModal");
+const signupModal = document.querySelector("#signupModal");
 const closeSignupModal = document.querySelector("#signup-btn");
 
 // function to display and hide login modal
 loginBtn.addEventListener("click", function() {
     loginModal.classList.add("modal");
 });
+
 closeLoginModal.addEventListener("click", function() {
     loginModal.classList.remove("modal");
 });
@@ -20,6 +19,7 @@ closeLoginModal.addEventListener("click", function() {
 signupBtn.addEventListener("click", function() {
     signupModal.classList.add("modal");
 });
+
 closeSignupModal.addEventListener("click", function() {
     signupModal.classList.remove("modal");
 });
