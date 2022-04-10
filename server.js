7b29d5b53f97e4e0da46f8ad67dcf426a3a3af76
Michael Cortez-Mejia <bayarea6fifty@gmail.com>
2022-04-10 01:17:51 -0700

    heroku fix

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const sequelize = require("./config/connection");
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
@@ -24,23 +25,16 @@ const helpers = require('./public/javascript/utils/helpers');
 
 app.use(session(sess));
 
-
-
-
 const hbs = exphbs.create({helpers});
-
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// pass in /public to make the image accessible in URL
-app.use( /*'public',*/ express.static(path.join(__dirname, 'public')));
 
 // engine must be set up after app intialization above 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// pass in /public to make the image accessible in URL
+app.use( /*'public',*/ express.static(path.join(__dirname, 'public')));
 
 // initiate routes 
 app.use(routes);
