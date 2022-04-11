5b4baf996c3cde9a44f5c9b5c2f40a428f77bdf0
Michael Cortez-Mejia <bayarea6fifty@gmail.com>
2022-04-10 23:09:42 -0700

    reseeded files

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -101,70 +101,7 @@ const postData = [
         title: '',
         post_body: '',
         category_name: 'Travel',
-    },
-{
-    title: 'Hello World 1',
-    post_body: ' a;sldkfjalser ja;lsdfja;woeirjff;laks ;aslldkfjl;asjdfoin ;alskdfjp;kasd jl;akjsdf sj ;lkasjdf;laksjdfopewa ;lkjasdfl kjaweoi ;laskdjfi ;lkajsfpoujeag ;lkajsdf asr',
-    category_name: 'Cars', 
-    user_id: 1,
-    // channel_id: 1
-},
-{
-    title: 'Hello World 2',
-    post_body: ' a;sldkfjalser ja;lsdfja;woeirjff;laks ;aslldkfjl;asjdfoin ;alskdfjp;kasd jl;akjsdf sj ;lkasjdf;laksjdfopewa ;lkjasdfl kjaweoi ;laskdjfi ;lkajsfpoujeag ;lkajsdf asr',
-    category_name: 'Gaming', 
-    user_id: 1,
-    // channel_id: 1
-},
-{
-    title: 'Hello World 3',
-    post_body: ' a;sldkfjalser ja;lsdfja;woeirjff;laks ;aslldkfjl;asjdfoin ;alskdfjp;kasd jl;akjsdf sj ;lkasjdf;laksjdfopewa ;lkjasdfl kjaweoi ;laskdjfi ;lkajsfpoujeag ;lkajsdf asr',
-    category_name: 'Gaming', 
-    user_id: 0,
-    // channel_id: 0
-},
-{
-    title: 'Hello World 4',
-    post_body: ' a;sldkfjalser ja;lsdfja;woeirjff;laks ;aslldkfjl;asjdfoin ;alskdfjp;kasd jl;akjsdf sj ;lkasjdf;laksjdfopewa ;lkjasdfl kjaweoi ;laskdjfi ;lkajsfpoujeag ;lkajsdf asr',
-    category_name: 'Gaming', 
-    user_id: 0,
-    // channel_id: 0
-},
-{
-    title: 'Hello World 5',
-    post_body: ' a;sldkfjalser ja;lsdfja;woeirjff;laks ;aslldkfjl;asjdfoin ;alskdfjp;kasd jl;akjsdf sj ;lkasjdf;laksjdfopewa ;lkjasdfl kjaweoi ;laskdjfi ;lkajsfpoujeag ;lkajsdf asr',
-    category_name: 'Gaming', 
-    user_id: 1,
-    // channel_id: 1
-},
-{
-    title: 'Hello World 6',
-    post_body: ' a;sldkfjalser ja;lsdfja;woeirjff;laks ;aslldkfjl;asjdfoin ;alskdfjp;kasd jl;akjsdf sj ;lkasjdf;laksjdfopewa ;lkjasdfl kjaweoi ;laskdjfi ;lkajsfpoujeag ;lkajsdf asr',
-    category_name: 'Gaming', 
-    user_id: 3,
-    // channel_id: 3
-},
-{
-    title: 'Hello World 7',
-    post_body: ' a;sldkfjalser ja;lsdfja;woeirjff;laks ;aslldkfjl;asjdfoin ;alskdfjp;kasd jl;akjsdf sj ;lkasjdf;laksjdfopewa ;lkjasdfl kjaweoi ;laskdjfi ;lkajsfpoujeag ;lkajsdf asr',
-    category_name: 'Crypto', 
-    user_id: 1,
-    // channel_id: 1
-},
-{
-    title: 'Hello World 8',
-    post_body: ' a;sldkfjalser ja;lsdfja;woeirjff;laks ;aslldkfjl;asjdfoin ;alskdfjp;kasd jl;akjsdf sj ;lkasjdf;laksjdfopewa ;lkjasdfl kjaweoi ;laskdjfi ;lkajsfpoujeag ;lkajsdf asr',
-    category_name: 'Crypto', 
-    user_id: 2,
-    // channel_id: 2
-},
-{
-    title: 'Hello World 8',
-    post_body: ' a;sldkfjalser ja;lsdfja;woeirjff;laks ;aslldkfjl;asjdfoin ;alskdfjp;kasd jl;akjsdf sj ;lkasjdf;laksjdfopewa ;lkjasdfl kjaweoi ;laskdjfi ;lkajsfpoujeag ;lkajsdf asr',
-    category_name: 'Business', 
-    user_id: 2,
-    // channel_id: 2
-},
+    }
 ];
 // function to import seeds into the db upon command 'npm run seed'
 const seedPosts = () => Post.bulkCreate(postData);
